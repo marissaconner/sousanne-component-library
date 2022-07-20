dd08a4c454dcb28d1704fd113db3eadfdae31b66
Marissa Conner <marissafromtexas@gmail.com>
2022-07-20 00:36:24 -0700

    Auggh I forgot to add it to the index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Link } from './Link'
 export { default as UList } from './UList'
 export { default as TextInput } from './Form/TextInput'
 export { default as Modal } from './Modal'
+export { default as Icon } from './Icon';
